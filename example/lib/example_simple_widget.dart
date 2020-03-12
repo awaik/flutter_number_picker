161975f00e1d5442758aad2f351f353633774fde
awaik <awaiking44@gmail.com>
2020-03-12 13:03:35 +0530

    initial

---
+++ example/lib/example_simple_widget.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_number_picker/flutter_number_picker.dart';
+
+void main() {
+  runApp(new ExampleApp());
+}
+
+class ExampleApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'NumberPicker Example',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'NumberPicker Example'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new FlutterNumberPicker.integer(
+                initialValue: _currentValue,
+                minValue: 0,
+                maxValue: 100,
+                horizontal: true,
+                onChanged: (newValue) =>
+                    setState(() => _currentValue = newValue)),
+            new Text("Current number: $_currentValue"),
+          ],
+        ),
+      ),
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+    );
+  }
+}
